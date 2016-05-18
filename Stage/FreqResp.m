f7bf94ba9d2cd0023398b1b49385ddd1a4eca743
Muhammad Imran <imranhsbm@gmail.com>
2016-05-18 14:17:14 +0200

    Nouveau Dossier Stage

--- Stage/FreqResp.m
+++ Stage/FreqResp.m
@@ -1,11 +1,13 @@
 
 clear 
-
 load fil.mat
 load process_D.mat
 load process_Mask.mat
 
 subplot(211)
 [y10_A,x10_A,Rr_Acc] = calculFreqResp_LapiAlgo(dataD_AC,fe);
+title('Accelerometre')
+
 subplot(212)
 [y10_M,x10_M,Rr_M] = calculFreqResp_LapiAlgo(dataMaskAC,fe);
+title('Mask')
